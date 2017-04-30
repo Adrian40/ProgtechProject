8eb3509282b0419a85392bf677239bda6784e738
ADMA <adrian.balogh@outlook.com>
2017-05-01 00:17:40 +0200

    Add Database

---
+++ AutoKolcsonzoApp/AutoKolcsonzoApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoKolcsonzoApp.MvcApplication" Language="C#" %>
---
+++ AutoKolcsonzoApp/AutoKolcsonzoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoKolcsonzoApp.Startup))]
+namespace AutoKolcsonzoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
